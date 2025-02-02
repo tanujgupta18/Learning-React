5084d4081d92c98efd33df5f909b9d7329b4c889
Tanuj Gupta <tanujgupta1872003@gmail.com>
2025-02-02 11:14:04 +0530

    Todo-final version

--- Projects/1-todo-app/src/App.jsx
+++ Projects/1-todo-app/src/App.jsx
@@ -1,27 +1,27 @@
 import AppName from "./Components/AppName";
 import AddTodo from "./Components/AddTodo";
 import TodoItems from "./Components/TodoItems";
+import { useState } from "react";
+import WelcomeMessage from "./Components/WelcomeMessage";
 
 function App() {
-  const todoItems = [
-    {
-      todoInput: "Learn Everyday",
-      todoDate: "31/01/2025",
-    },
-    {
-      todoInput: "Go to College",
-      todoDate: "31/01/2025",
-    },
-    {
-      todoInput: "Complete Assignments",
-      todoDate: "01/02/2025",
-    },
-  ];
+  const [todoItems, setTodoItems] = useState([]);
+
+  const handleNewItem = (itemName, itemDate) => {
+    // console.log(`New Item Added: ${itemName} Date: ${itemDate}`);
+    const newTodoItems = [
+      ...todoItems,
+      { todoInput: itemName, todoDate: itemDate },
+    ];
+    setTodoItems(newTodoItems);
+  };
+
   return (
     <center className="todo-container flex justify-center p-4">
       <div className="text-center">
         <AppName />
-        <AddTodo />
+        <AddTodo onNewItem={handleNewItem} />
+        {todoItems.length === 0 && <WelcomeMessage />}
         <TodoItems todoItems={todoItems} />
       </div>
     </center>
--- Projects/1-todo-app/src/Components/AddTodo.jsx
+++ Projects/1-todo-app/src/Components/AddTodo.jsx
@@ -1,11 +1,34 @@
-function AddTodo() {
+import { useState } from "react";
+
+function AddTodo({ onNewItem }) {
+  const [todoName, setTodoName] = useState("");
+  const [dueDate, setDueDate] = useState("");
+
+  const handleNameChange = (event) => {
+    setTodoName(event.target.value);
+  };
+  const handleDateChange = (event) => {
+    setDueDate(event.target.value);
+  };
+
+  const handleAddButtonClicked = () => {
+    if (!todoName || !dueDate) {
+      alert("Please fill out both fields!");
+      return;
+    }
+    onNewItem(todoName, dueDate);
+    setDueDate("");
+    setTodoName("");
+  };
   return (
-    <div className="grid grid-cols-12 gap-4">
+    <div className="grid grid-cols-12 gap-4 mb-4">
       <div className="col-span-6">
         <input
           type="text"
           placeholder="Enter Todo here"
           className="w-full px-4 py-2 border border-gray-300 rounded-lg"
+          value={todoName}
+          onChange={handleNameChange}
         />
       </div>
       <div className="col-span-4">
@@ -13,12 +36,15 @@ function AddTodo() {
           type="date"
           id="todoDate"
           className="w-full px-4 py-2 border border-gray-300 rounded-lg"
+          value={dueDate}
+          onChange={handleDateChange}
         />
       </div>
       <div className="col-span-2 flex justify-center items-center">
         <button
           type="submit"
           className="w-full px-6 py-2 rounded-lg bg-green-500 text-white"
+          onClick={handleAddButtonClicked}
         >
           Add
         </button>

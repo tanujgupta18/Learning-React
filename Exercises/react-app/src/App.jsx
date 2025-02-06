d209db72c64331dfc257771e85f5c377e6062988
Tanuj Gupta <tanujgupta1872003@gmail.com>
2025-02-06 12:06:54 +0530

    Exercise 1 - Solved

---
+++ Exercises/react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Exercise1 from "./Components/Exercise1";
+
+const App = () => {
+  return (
+    <div>
+      <Exercise1 />
+    </div>
+  );
+};
+
+export default App;

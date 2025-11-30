2e6219b8c2b7831082c32ee6e6ca27cdd441acea
Tanuj Gupta <tanujgupta1872003@gmail.com>
2025-12-01 00:08:34 +0530

    Dyanamic Routing - useNavigate

--- Practise-2.0/advanced-routing/src/pages/About.jsx
+++ Practise-2.0/advanced-routing/src/pages/About.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const About = () => {
+  const navigate = useNavigate();
+
+  const btnClicked = () => {
+    navigate("/");
+  };
+
   return (
     <div>
+      <button
+        onClick={btnClicked}
+        className="bg-emerald-600 px-5 py-2 rounded m-2 cursor-pointer active:scale-95"
+      >
+        Return to Home Page
+      </button>
       <h1>About Page</h1>
     </div>
   );

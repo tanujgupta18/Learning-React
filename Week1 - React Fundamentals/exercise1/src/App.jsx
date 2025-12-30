b49ca44fb5e525de859ca0d2e277df188d141923
Tanuj Gupta <tanujgupta1872003@gmail.com>
2025-12-30 11:41:32 +0530

    Exercise 1

---
+++ Week1 - React Fundamentals/exercise1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hello from "./components/Hello";
+
+const App = () => {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+};
+
+export default App;
---
+++ Week1 - React Fundamentals/exercise1/src/components/Hello.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hello = () => {
+  return <div>Hello, React</div>;
+};
+
+export default Hello;
